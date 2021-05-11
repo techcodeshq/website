f1102b0135544345789ef7857561f80590274a22
SantiagoVira <8bitharrypotter@gmail.com>
2021-05-11 18:13:40 -0400

    summer camp page

---
+++ src/pages/summer-camp.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { Hero, Description } from '@components/event';
+import { CTA } from '@components/home';
+import Summer from '@images/codefest/codefest.svg';
+
+const SummerCamp = () => {
+  return (
+    <>
+      <Helmet title="Summer Camp" />
+      <div data-scroll-section>
+        <Hero name="Summer Camp" date="Your Mother O' Clock" src={Summer} />
+      </div>
+      <div data-scroll-section>
+        <Description
+          rsvp
+          signup="https://www.w3schools.com/cssref/css3_pr_word-wrap.asp"
+          description="hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+        />
+      </div>
+      <div data-scroll-section>
+        <CTA />
+      </div>
+    </>
+  );
+};
+
+export default SummerCamp;
