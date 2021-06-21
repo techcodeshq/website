78d91d5d2ebc5869b8f9f099633ab36b241916a4
Kyrylo Orlov <dev@kyryloorlov.com>
2021-06-21 10:05:18 -0400

    Added footer

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as SmoothScroll } from './smoothScroll';
 export { ThemeProvider, ThemeContext } from './ThemeContext';
 export { default as Nav } from './nav';
 export { default as HTMLRenderer } from './parser';
+export { default as Footer } from './footer';
